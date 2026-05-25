acb6381f2678fc0b52ec1aae93e51909fc36b25c
Jonathan <jonathans.hernandez@autonoma.edu.co>
2026-05-25 02:04:31 -0500

    Agrego jugador y movimiento basico

--- Proyecto_final/vista/Escenario.java
+++ Proyecto_final/vista/Escenario.java
@@ -1,45 +1,37 @@
 package Proyecto_final.vista;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Objects;
 
 public class Escenario {
 
+    // Imagen del fondo
+    private BufferedImage fondo;
+
+    // Constructor
+    public Escenario() {
+
+        try {
+
+            fondo = ImageIO.read(
+                    new File("/Users/Jonathan/Documents/poo/Proyecto_final/resources/escenarios/fondo.png")
+            );
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+
+        }
+    }
+
+    // Método para dibujar
     public void draw(Graphics g) {
 
-        // CIELO
+        g.drawImage(fondo, 0, 0, 1500, 1000, null);
 
-        g.setColor(new Color(15, 15, 30));
-
-        g.fillRect(0, 0, 1500, 1000);
-
-        // LUNA
-
-        g.setColor(Color.WHITE);
-
-        g.fillOval(600, 50, 100, 100);
-
-        // EDIFICIOS
-
-        g.setColor(new Color(20, 20, 20));
-
-        g.fillRect(50, 200, 100, 300);
-
-        g.fillRect(200, 150, 120, 350);
-
-        g.fillRect(400, 180, 150, 320);
-
-        g.fillRect(620, 220, 130, 280);
-
-        // PISO
-
-        g.setColor(new Color(40, 40, 40));
-
-        g.fillRect(0, 500, 800, 100);
-
-        // LINEA DEL PISO
-
-        g.setColor(Color.YELLOW);
-
-        g.fillRect(0, 495, 800, 5);
     }
 }
